2fcf0bc920b495ad5c24f02fa6cc10895774be22
tp6vup54 <genius091612@gmail.com>
2017-02-28 21:04:59 +0800

    Refine review transparent

--- ui/label.py
+++ ui/label.py
@@ -4,23 +4,24 @@ import os
 class MangaPreviewLabel(QtWidgets.QLabel):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self._max_size = QtCore.QSize(960, 1080)
         self.setGeometry(QtCore.QRect(0, 0, 960, 1080))
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setScaledContents(True)
-        # self.setAutoFillBackground(False)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
 
     def set_image_path(self, path):
         if os.path.isfile(path):
             img = QtGui.QImage(path)
+            img = img.scaled(self._max_size, QtCore.Qt.KeepAspectRatio)
+            img = img.convertToFormat(QtGui.QImage.Format_ARGB32)
             p = QtGui.QPainter()
             p.begin(img)
             p.setCompositionMode(QtGui.QPainter.CompositionMode_DestinationIn)
             p.fillRect(img.rect(), QtGui.QColor(0, 0, 0, 100))
             p.end()
             pixmap = QtGui.QPixmap.fromImage(img)
-            scaledPix = pixmap.scaled(self.size(), QtCore.Qt.KeepAspectRatio)
 
-            self.setMaximumSize(scaledPix.size())
-            self.setPixmap(scaledPix)
+            self.setMaximumSize(pixmap.size())
+            self.setPixmap(pixmap)
